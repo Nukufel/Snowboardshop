923ee3d8fa2c1dec0928eb4b7ee8b11853e60169
Nukufel <vogeln@bzz.ch>
2022-05-18 11:10:28 +0200

    Snowboard atribute und get/set Shop atribute und get/set

--- src/main/java/ch/bzz/snowboardshop/model/Snowboard.java
+++ src/main/java/ch/bzz/snowboardshop/model/Snowboard.java
@@ -6,4 +6,44 @@ public class Snowboard {
     private String snowboardArt;
     private Double snowboardPreis;
     private String snowboardMarke;
+
+    public String getSnowboardUUID() {
+        return snowboardUUID;
+    }
+
+    public void setSnowboardUUID(String snowboardUUID) {
+        this.snowboardUUID = snowboardUUID;
+    }
+
+    public Double getSnowboardLänge() {
+        return snowboardLänge;
+    }
+
+    public void setSnowboardLänge(Double snowboardLänge) {
+        this.snowboardLänge = snowboardLänge;
+    }
+
+    public String getSnowboardArt() {
+        return snowboardArt;
+    }
+
+    public void setSnowboardArt(String snowboardArt) {
+        this.snowboardArt = snowboardArt;
+    }
+
+    public Double getSnowboardPreis() {
+        return snowboardPreis;
+    }
+
+    public void setSnowboardPreis(Double snowboardPreis) {
+        this.snowboardPreis = snowboardPreis;
+    }
+
+    public String getSnowboardMarke() {
+        return snowboardMarke;
+    }
+
+    public void setSnowboardMarke(String snowboardMarke) {
+        this.snowboardMarke = snowboardMarke;
+    }
 }
