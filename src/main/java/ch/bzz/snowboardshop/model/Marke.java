347c2ce051211241dba40a678f678940727415e5
Nukufel <vogeln@bzz.ch>
2022-05-18 11:11:43 +0200

    Snowboard atribute und get/set Shop atribute und get/set Marke atribute und get/set

--- src/main/java/ch/bzz/snowboardshop/model/Marke.java
+++ src/main/java/ch/bzz/snowboardshop/model/Marke.java
@@ -1,5 +1,22 @@
 package ch.bzz.snowboardshop.model;
 
 public class Marke {
+    private String markeUUID;
+    private String markeName;
 
+    public String getMarkeUUID() {
+        return markeUUID;
+    }
+
+    public void setMarkeUUID(String markeUUID) {
+        this.markeUUID = markeUUID;
+    }
+
+    public String getMarkeName() {
+        return markeName;
+    }
+
+    public void setMarkeName(String markeName) {
+        this.markeName = markeName;
+    }
 }
