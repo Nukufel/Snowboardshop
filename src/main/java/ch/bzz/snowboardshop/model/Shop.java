585d351f106ea56cbaa54b77925be6467a1e3480
Nukufel <vogeln@bzz.ch>
2022-06-10 12:20:11 +0200

    do regex for uuid

--- src/main/java/ch/bzz/snowboardshop/model/Shop.java
+++ src/main/java/ch/bzz/snowboardshop/model/Shop.java
@@ -1,5 +1,7 @@
 package ch.bzz.snowboardshop.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import javax.ws.rs.FormParam;
@@ -8,18 +10,27 @@ import java.util.List;
 
 public class Shop {
     private String shopUUID;
+
     @Size(min=1, max=100)
     @FormParam("shopName")
     private String shopName;
-    //@Pattern(regexp = "0(2[1-246-7]|3[1-4]|4[13-4]|5[25-6]|6[1-2]|7[15-68-9]|8[17]|91)[0-9]{7}")
+
+
+    @Pattern(regexp = "0(2[1-246-7]|3[1-4]|4[13-4]|5[25-6]|6[1-2]|7[15-68-9]|8[17]|91)[0-9]{7}")
     @FormParam("shopTel")
     private String shopTel;
+
+
     @Size(min=1, max=100)
     @FormParam("shopAdresse")
     private String shopAdresse;
+
     @Size(min=1, max=15)
     @FormParam("shopPLZ")
     private String shopPLZ;
+
+    //@Pattern(regexp="[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}")
+    @FormParam("snowboardUUIDList")
     private List<String> snowboardUUIDList = new ArrayList<>();
 
     /**
